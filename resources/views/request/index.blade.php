349b4a661edc70ac63172d643753591badbabbeb
Rizky Adi Pratama <71449366+rzkadp@users.noreply.github.com>
2022-09-24 08:07:22 +0700

    Update index.blade.php

--- resources/views/request/index.blade.php
+++ resources/views/request/index.blade.php
@@ -20,7 +20,6 @@
                             <option>2</option>
                             <option>3</option>
                             <option>4</option>
-                            <option>5</option>
                         </select>
                     </div>
                 </div>
